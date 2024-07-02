b53985e060dc98cb95e61b4573d19af41c17a1cb
Ian Speelman <ianspeelman@gmail.com>
2024-07-02 19:41:55 +0200

    not classified table shows only relevant info, fastest lap is determined

--- app.py
+++ app.py
@@ -20,5 +20,13 @@ def upload():
     with pdfplumber.open(filename) as pdf:
         page = pdf.pages[1];
         results = page.extract_tables(table_settings={"snap_x_tolerance": 1})
-        # return results
-    return render_template("table.html", results=results)
+    fastest_index = 0
+    i = 0
+    for result in results[0]:
+        if result < results[0][fastest_index]:
+            fastest_index = i
+        i += 1
+    
+    # return fastest_lap
+    fastest_lap = results[0][fastest_index]
+    return render_template("table.html", results=results, fastest_lap=fastest_lap)
