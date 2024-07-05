25be3308914891919aba195a30266468d441fc05
Ian Speelman <ianspeelman@gmail.com>
2024-07-05 11:36:54 +0200

    Add gathered info to database
    adds drivers to database if they do not yet exist
    adds teams to database if they do not yet exist
    links teams and drivers together, if that is not yet the case
    removed get fastest lap function for now (breaking code changes, and no priority for now)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect
-from helpers import getData
+from helpers import getData, checkDriver
 
 
 
@@ -15,9 +15,14 @@ def index():
 def upload():
     results = getData(request.files["results"])
     drivers = getData(request.files["drivers"])
-    # fastest_lap = getFastestLap(results)
     data = {}
     data["results"] = results
     data["drivers"] = drivers
+
+    for i in range(len(data["drivers"])):
+        if data["drivers"][i][0] != "" and data["drivers"][i][0] != "No.":
+            checkDriver(data["drivers"][i])
     return data
+    return data["drivers"][2]
     return render_template("table.html", results=results, fastest_lap=results[0][fastest_lap])
+
