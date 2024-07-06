acd834b064d660f9d4dcbc26e09926017ca411b8
Ian Speelman <ianspeelman@gmail.com>
2024-07-06 12:28:55 +0200

    Add events
    Events can now be added
    events get retrieved when uploading results so you can choose a event to wich the results belong to
    when uploading result files only the correct data will be shown on the confimation page, with the option to edit

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
 from flask import Flask, request, render_template, redirect
-from helpers import getData, checkDriver
-
+from helpers import getData, checkDriver, getEvents, createEvent, getTracks
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    events = getEvents()
+    return render_template("index.html", events=events)
 
+@app.route("/createevent", methods=["GET", "POST"])
+def makeevent():
+    if request.method == "POST":
+        createEvent(request.form)
+        return redirect("/createevent")
+    else: 
+        tracks = getTracks()
+        return render_template("createevent.html", tracks=tracks)
 
-
-@app.route("/upload", methods=["POST"])
+@app.route("/upload", methods=["GET", "POST"])
 def upload():
-    results = getData(request.files["results"])
-    drivers = getData(request.files["drivers"])
-    data = {}
-    data["results"] = results
-    data["drivers"] = drivers
+    if request.method == "POST":
+        results = getData(request.files["results"],"1,2")
+        drivers = getData(request.files["drivers"],"2")
+        data = {}
+        data["meta"] = results[1]
+        data["results"] = []
+        data["drivers"] = drivers[0]
+        for result in results[0]:
+            try:
+                if int(result[4]):
+                    data["results"].append(result)
+            except:
+                continue
 
-    for i in range(len(data["drivers"])):
-        if data["drivers"][i][0] != "" and data["drivers"][i][0] != "No.":
-            checkDriver(data["drivers"][i])
-    return data
-    return data["drivers"][2]
-    return render_template("table.html", results=results, fastest_lap=results[0][fastest_lap])
+        for i in range(len(data["drivers"])):
+            if data["drivers"][i][0] != "" and data["drivers"][i][0] != "No.":
+                checkDriver(data["drivers"][i])
+        return render_template("confirm.html", results=data["results"])
+    else:
+        events = getEvents()
+        return render_template("index.html", events=events)
 
